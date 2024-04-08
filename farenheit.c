ff35ff7eec0363c616a6d7a771ed4c6ca0b3c39b
misnathomas <misnathomas2005@gmail.com>
2024-04-08 21:26:22 +0530

    Create farenheit.c

---
+++ farenheit.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+    float c,f;
+    printf("Enter the temperature in celsius");
+    scanf("%f",&c);
+    f=c*1.8+32;
+    printf("Temperature in farenheit is %f",f);
+}
